9409413707fb81f51915316637d90b7291920191
nastprol <nastprol@users.noreply.github.com>
2018-11-25 16:54:18 +0500

    synchronized

--- src/db/DataBase.java
+++ src/db/DataBase.java
@@ -34,12 +34,6 @@ public class DataBase implements IDataBase {
 
 	public void initDatabase() {
 		try {
-			try {
-				Class.forName("org.sqlite.JDBC");
-			} catch(ClassNotFoundException ex) {
-				ex.printStackTrace();
-				System.exit(1);
-			}
 			String dbUrl = System.getenv("DB_URL");
 			c = DriverManager.getConnection(dbUrl);
 			if (c.isClosed())
