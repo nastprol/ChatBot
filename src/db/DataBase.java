1fb9a197f143f38db382b5c2e593964e3dd5ffd2
nastprol <nastprol@users.noreply.github.com>
2018-12-03 22:13:42 +0500

    added JDBC

--- src/db/DataBase.java
+++ src/db/DataBase.java
@@ -14,7 +14,7 @@ public class DataBase implements IDataBase {
 
 	public void connect() {
 		try {
-			String dbUrl = System.getenv("DATABASE_URL");
+			String dbUrl = System.getenv("JDBC_DATABASE_URL");
 			c = DriverManager.getConnection(dbUrl);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -35,7 +35,7 @@ public class DataBase implements IDataBase {
 	public void initDatabase() {
 		try {
 			
-			String dbUrl = System.getenv("DATABASE_URL");
+			String dbUrl = System.getenv("JDBC_DATABASE_URL");
 			c = DriverManager.getConnection(dbUrl);
 			if (c.isClosed())
 				tryConnect();
